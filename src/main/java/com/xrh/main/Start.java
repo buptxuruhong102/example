f70f808b315edb33de27d8161fca3fec66922096
buptxuruhong102 <buptxuruhong102@163.com>
2018-04-30 15:35:42 +0800

    timeservice

---
+++ src/main/java/com/xrh/main/Start.java
@@ -0,0 +1,14 @@
+package com.xrh.main;
+
+import com.xrh.utils.TimeService;
+
+/**
+ * Created by buptxuruhong on 2018/4/30.
+ */
+public class Start {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            System.out.println(TimeService.currentTimeMillis());
+        }
+    }
+}
--- src/main/java/com/xrh/utils/TimeService.java
+++ src/main/java/com/xrh/utils/TimeService.java
@@ -1,7 +1,27 @@
 package com.xrh.utils;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
+ * 解决多线程并发调用System.currentTimeMillis()性能问题
  * Created by buptxuruhong on 2018/4/30.
  */
 public class TimeService {
+    private static long currentTimeMillis = System.currentTimeMillis();
+    private static ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+    public static long currentTimeMillis(){
+        return currentTimeMillis;
+    }
+
+    static {
+        executorService.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                currentTimeMillis = System.currentTimeMillis();
+            }
+        }, 0, 1, TimeUnit.MILLISECONDS);
+    }
 }
+
