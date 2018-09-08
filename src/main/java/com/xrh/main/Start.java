bd9ee5c0cbbc5690f9ab01e00e454fb8d4baf6b9
xuruhong <xuruhong@jd.com>
2018-09-08 12:27:14 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/xrh/main/Start.java

--- src/main/java/com/xrh/main/Start.java
+++ src/main/java/com/xrh/main/Start.java
@@ -9,6 +9,7 @@ public class Start {
     public static void main(String[] args) {
         for(int i = 0; i < 10; i++){
             System.out.println(TimeService.currentTimeMillis() + "1231");
+            System.out.println(TimeService.currentTimeMillis() + "123145");
         }
     }
 }
